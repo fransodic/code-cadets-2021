8b693f1215c11126b2db4de10d41db3e24295063
Fran Sodić <fran.sodic@fer.hr>
2021-06-04 13:23:46 +0200

    duplicate files deleted

--- lecture_3/03_project/calculator/cmd/bootstrap/RabbitMq.go
+++ lecture_3/03_project/calculator/cmd/bootstrap/RabbitMq.go
@@ -3,7 +3,7 @@ package bootstrap
 import "github.com/streadway/amqp"
 
 func RabbitMq() *amqp.Channel {
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672")
 	if err != nil {
 		panic(err)
 	}
